897f79110797ae46db3128b5706e74ad022b329f
senku0-0 <ashadshaikh.zx@gmail.com>
2026-03-13 13:25:26 +0530

    Registration Repo done

--- src/main/java/com/buskarro/Buskarro/Repository/RegistrationRepo.java
+++ src/main/java/com/buskarro/Buskarro/Repository/RegistrationRepo.java
@@ -1,4 +1,13 @@
 package com.buskarro.Buskarro.Repository;
 
-public interface RegistrationRepo {
+import com.buskarro.Buskarro.Model.RegistrationFields;
+import org.bson.types.ObjectId;
+import org.springframework.data.mongodb.repository.MongoRepository;
+
+import java.util.Optional;
+
+public interface RegistrationRepo extends MongoRepository<RegistrationFields, ObjectId> {
+    Optional<RegistrationFields> findByEmail(String email);
+    Optional<RegistrationFields> findByUsername(String username);
+    Optional<RegistrationFields> deleteByEmail(String email);
 }
