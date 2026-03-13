6cf030b83998fece02ebd820679ab47a115b4809
senku0-0 <ashadshaikh.zx@gmail.com>
2026-03-13 13:26:46 +0530

    Registration REST Api's

--- src/main/java/com/buskarro/Buskarro/Controller/RegistrationController.java
+++ src/main/java/com/buskarro/Buskarro/Controller/RegistrationController.java
@@ -1,4 +1,37 @@
 package com.buskarro.Buskarro.Controller;
 
+import com.buskarro.Buskarro.Model.RegistrationFields;
+import com.buskarro.Buskarro.Service.RegistrationService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/Register")
 public class RegistrationController {
+    @Autowired
+    private RegistrationService service;
+
+    @GetMapping
+    public ResponseEntity<?> getAllUsers(){
+        try {
+            return new ResponseEntity<>(service.getAllUsers(), HttpStatus.OK);
+        }catch (Exception e) {
+            return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+    }
+
+    @PostMapping
+    public ResponseEntity<?> saveUser(@RequestBody RegistrationFields entry){
+        try {
+            service.saveUser(entry);
+            return new ResponseEntity<>(HttpStatus.OK);
+        } catch (Exception e) {
+            return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
+        }
+    }
+
 }
