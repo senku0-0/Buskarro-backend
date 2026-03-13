9d0881c9158b776a953ad36e0f7e18f35e5ef410
senku0-0 <ashadshaikh.zx@gmail.com>
2026-03-13 14:14:46 +0530

    Customized Spring Security

--- src/main/java/com/buskarro/Buskarro/Config/SpringSecurity.java
+++ src/main/java/com/buskarro/Buskarro/Config/SpringSecurity.java
@@ -1,4 +1,26 @@
 package com.buskarro.Buskarro.Config;
 
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.web.SecurityFilterChain;
+
+
+@Configuration
+@EnableWebSecurity
 public class SpringSecurity {
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http
+                .csrf(AbstractHttpConfigurer::disable) // disable CSRF
+                .httpBasic(Customizer.withDefaults()) // enable Basic Auth
+                .authorizeHttpRequests(auth -> auth
+                        .requestMatchers("/Register/**").permitAll() // open /Register
+                        .anyRequest().authenticated() // everything else requires login
+                );
+        return http.build();
+    }
 }
