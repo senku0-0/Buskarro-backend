1dd18107e6c2949ae111dad00401dbc955f374ef
senku0-0 <ashadshaikh.zx@gmail.com>
2026-03-13 13:26:07 +0530

    Registration table(Dcoument) is done

--- src/main/java/com/buskarro/Buskarro/Model/RegistrationFields.java
+++ src/main/java/com/buskarro/Buskarro/Model/RegistrationFields.java
@@ -1,4 +1,22 @@
 package com.buskarro.Buskarro.Model;
 
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.bson.types.ObjectId;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.index.Indexed;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+@Document
+@Data
+@NoArgsConstructor
 public class RegistrationFields {
+    @Id
+    private ObjectId id;
+    @Indexed(unique = true)
+    private String email;
+    @Indexed(unique = true)
+    private String username;
+    private String phoneno;
+    private String password;
 }
