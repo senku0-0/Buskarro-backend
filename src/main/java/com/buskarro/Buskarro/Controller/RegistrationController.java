a8bf0725f76b4ea1149eb083038c418e27635709
senku0-0 <ashadshaikh.zx@gmail.com>
2026-04-12 11:59:28 +0530

    Created Session tracking and improved security

--- src/main/java/com/buskarro/Buskarro/Controller/RegistrationController.java
+++ src/main/java/com/buskarro/Buskarro/Controller/RegistrationController.java
@@ -1,15 +1,21 @@
 package com.buskarro.Buskarro.Controller;
 
-import com.buskarro.Buskarro.Config.SpringSecurity;
+import com.buskarro.Buskarro.DTO.RegistrationDTO;
 import com.buskarro.Buskarro.Model.RegistrationFields;
+import com.buskarro.Buskarro.Model.SessionFields;
+import com.buskarro.Buskarro.Repository.SessionRepo;
 import com.buskarro.Buskarro.Service.RegistrationService;
+import com.buskarro.Buskarro.Utility.JWTUtility;
+import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.List;
+import java.time.LocalDateTime;
+import java.util.Map;
+
 
 @RestController
 @RequestMapping("/Register")
@@ -18,6 +24,10 @@ public class RegistrationController {
     private RegistrationService service;
     @Autowired
     private PasswordEncoder passwordEncoder;
+    @Autowired
+    SessionRepo ses;
+    @Autowired
+    private JWTUtility jwtUtility;
 
     @GetMapping
     public ResponseEntity<?> getAllUsers(){
@@ -29,18 +39,32 @@ public class RegistrationController {
     }
 
     @PostMapping
-    public ResponseEntity<?> saveUser(@RequestBody RegistrationFields req){
+    public ResponseEntity<?> saveUser(@RequestBody RegistrationDTO req, HttpServletRequest request){
         try {
+            RegistrationFields user = new RegistrationFields();
             String password = req.getPassword();
             String encode_password = passwordEncoder.encode(password);
-            req.setPassword(encode_password);
-            req.setRole("USER");
-            service.saveUser(req);
+            user.setName(req.getName());
+            user.setEmail(req.getEmail());
+            user.setPhone(req.getPhone());
+            user.setPassword(encode_password);
+            user.setRole("DEVELOPER");
+            // Save user first
+            RegistrationFields savedUser = service.saveUser(user);
+            // Create session (auto-login)
+            SessionFields session = new SessionFields();
+            String getJwt = jwtUtility.generateToken(savedUser.getId(), savedUser.getEmail(), savedUser.getRole(), savedUser.getName() );
+            session.setUserId(savedUser.getId());
+            session.setToken(getJwt);
+            session.setCreatedAt(LocalDateTime.now()); // store current time
+            session.setExpiresAt(LocalDateTime.now().plusHours(24)); // 24 hrs of session
+            session.setIpAddress(request.getRemoteAddr()); // user ip Address
+            session.setActive(true);
+            ses.save(session);
             System.out.println("User saved successfully!");
-            return new ResponseEntity<>(HttpStatus.OK);
+            return ResponseEntity.ok(Map.of("jwt_token",getJwt));
         } catch (Exception e) {
             return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
         }
     }
-
 }
