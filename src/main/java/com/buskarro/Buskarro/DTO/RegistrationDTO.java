44fa48e0ddd62205675878f67c4b802b302c6c38
senku0-0 <ashadshaikh.zx@gmail.com>
2026-04-12 12:01:17 +0530

    Using DTO(DATA TRANSFER OBJECT) rather than using RegistrationFeilds directly to make it more flexible

--- src/main/java/com/buskarro/Buskarro/DTO/RegistrationDTO.java
+++ src/main/java/com/buskarro/Buskarro/DTO/RegistrationDTO.java
@@ -1,4 +1,13 @@
 package com.buskarro.Buskarro.DTO;
 
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
 public class RegistrationDTO {
+    private String name;
+    private String phone;
+    private String email;
+    private String password;
 }
