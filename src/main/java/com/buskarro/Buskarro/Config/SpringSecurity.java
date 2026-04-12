ed9c61128a918663a7cd9ac2582c232ac89fec1b
senku0-0 <ashadshaikh.zx@gmail.com>
2026-04-12 11:58:47 +0530

    live Buskarro url added

--- src/main/java/com/buskarro/Buskarro/Config/SpringSecurity.java
+++ src/main/java/com/buskarro/Buskarro/Config/SpringSecurity.java
@@ -37,7 +37,7 @@ public class SpringSecurity {
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
-        configuration.setAllowedOrigins(Arrays.asList("http://localhost:5173")); // Your React app URL
+        configuration.setAllowedOrigins(Arrays.asList("http://localhost:5173","https://buskarro.vercel.app")); // Your React app URL
         configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE", "OPTIONS"));
         configuration.setAllowedHeaders(Arrays.asList("*"));
         configuration.setAllowCredentials(true);
