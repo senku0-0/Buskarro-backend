2674e9886337d2d0b9b98853f5e03aa7e6958947
senku0-0 <ashadshaikh.zx@gmail.com>
2026-04-12 12:03:43 +0530

    Session Repo

--- src/main/java/com/buskarro/Buskarro/Repository/SessionRepo.java
+++ src/main/java/com/buskarro/Buskarro/Repository/SessionRepo.java
@@ -1,4 +1,8 @@
 package com.buskarro.Buskarro.Repository;
 
-public interface SessionRepo {
+import com.buskarro.Buskarro.Model.SessionFields;
+import org.bson.types.ObjectId;
+import org.springframework.data.mongodb.repository.MongoRepository;
+
+public interface SessionRepo extends MongoRepository<SessionFields, ObjectId> {
 }
